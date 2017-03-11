8576119b2fcdfffc1c56fce87b6bfdd74225205f
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-03-11 13:55:16 +0300

    Init core and posts applications.

--- application/settings.py
+++ application/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core.apps.CoreConfig',
+    'posts.apps.PostsConfig'
 ]
 
 MIDDLEWARE = [
