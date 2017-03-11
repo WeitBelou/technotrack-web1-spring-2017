acc75445048f87be26741780fd9b83c31e0a000c
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-03-11 14:09:21 +0300

    Add user models.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User
+
+
+class UserAdmin(BaseUserAdmin):
+
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Дополнительно', {'fields': ('admin_avatar', 'avatar')}),
+    )
+
+    def admin_avatar(self, instance):
+        return instance.avatar and '<img src="{0}" width="100px" />'.format(
+            instance.avatar.url
+        )
+    admin_avatar.allow_tags = True
+    admin_avatar.short_description = 'Аватар'
+
+
+admin.site.register(User, UserAdmin)
