5f780a8537d7d287d46da588c0b19842c603d620
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-04-02 01:33:48 +0300

    Add bootstrap templates.

--- application/settings.py
+++ application/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
     'core.apps.CoreConfig',
     'blogs.apps.BlogsConfig',
     'comments.apps.CommentsConfig',
