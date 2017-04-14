aa39ca6a0ebf07ef5489a07bf57ad722411907d7
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-04-14 22:10:45 +0300

    Move all jquerry actions to document.ready

--- core/static/core/js/core.js
+++ core/static/core/js/core.js
@@ -1,17 +1,14 @@
-$(document).ready(
-    function () {
+$(function () {
         $('.autoload').each(function () {
             $(this).load($(this).attr('data-url'));
         });
+
         setInterval(function () {
             $('.autoload').each(function () {
                 $(this).load($(this).attr('data-url'))
             });
         }, 3000);
-    }
-);
 
-$(function () {
         $('.load-on-click').click(function () {
             var data = $(this).data();
             $(data.destination).load(data.url);
