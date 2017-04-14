060726c9ab20ef2c49b2b68b417569d98ae1afcb
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-04-14 06:42:37 +0300

    Add load on click action.

--- core/static/core/js/core.js
+++ core/static/core/js/core.js
@@ -2,6 +2,13 @@ $(document).ready(
     function () {
         $('.autoload').each(function () {
             $(this).load($(this).attr('data-url'));
-        })
+        });
+    }
+);
+
+$(function () {
+        $('.load-on-click').click(function () {
+            $(this).load($this).attr('data-url');
+        });
     }
 );
