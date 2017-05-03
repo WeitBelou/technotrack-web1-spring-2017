1ea0ee0a9bc662e18ed133b99f617d0cc420b555
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-05-03 21:16:30 +0300

    Simplify logger.

--- application/settings.py
+++ application/settings.py
@@ -93,49 +93,16 @@ DATABASES = {
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '[%{levelname}s] %{asctime}s %{module}s %{process}d %{thread}d %{message}s'
-        },
-        'simple': {
-            'format': '[%{levelname}s] %{message}s'
-        },
-    },
-    'filters': {
-        'require_debug_true': {
-            '()': 'django.utils.log.RequireDebugTrue',
-        }
-    },
     'handlers': {
         'console': {
-            'level': 'INFO',
-            'filters': ['require_debug_true'],
             'class': 'logging.StreamHandler',
         },
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler',
-        },
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': os.environ.get('LOG_FILE', default=os.path.join(BASE_DIR, 'logs/debug.log')),
-        }
     },
     'loggers': {
         'django': {
             'handlers': ['console'],
-            'propagate': True
+            'level': os.environ.get('DJANGO_LOG_LEVEL', 'ERROR')
         },
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': False,
-        },
-        'web_track.custom': {
-            'handlers': ['console', 'mail_admins'],
-            'level': 'INFO'
-        }
     }
 }
 
